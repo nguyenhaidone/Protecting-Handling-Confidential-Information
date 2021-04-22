0446c272ed6baa474f656e7f02684f17ce85018e
nguyenhaidone <dang682000@gmail.com>
2021-04-22 11:09:15 +0700

    ha_bac_2

--- Algorithm/Modulo/DescExponentialModulo.cpp
+++ Algorithm/Modulo/DescExponentialModulo.cpp
@@ -1,8 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int getValue(int a, int m, int n){
-	int arr[100];
+int ha_bac_1(int a, int m, int n){
+	int arr[100];//luu so du
 	
 	int x=0;
 	while(m>3){
@@ -30,14 +30,20 @@ int getValue(int a, int m, int n){
 	return t;
 }
 
+double ha_bac_2(int a, int m, int n) {
+	if (m <= 3)
+		return (long)pow(a, m) % n;
+		
+	if (m % 2 == 0) {
+		double sub = ha_bac_2(a, m / 2, n);
+		return (long)pow(sub, 2) % n;
+	}
+	double sub = ha_bac_2(a, (m - 1) / 2, n);
+	return (((long)pow(sub, 2) % n) * (a % n)) % n;
+}
+
 int main(){
-	int a,m,n;
-	
-	cout<<"b=a^m mod n\n";
-	cout<<"Nhap a: ";cin>>a;
-	cout<<"Nhap m: ";cin>>m;
-	cout<<"Nhap n: ";cin>>n;
-	cout<<"\nha bac luy thua:";
-	
-	cout<<"\nb= "<<getValue(a,m,n);
+	cout << ha_bac_1(281, 764, 6967) << endl;
+	cout << ha_bac_2(281, 764, 6967) << endl;
+	return 0;
 }
